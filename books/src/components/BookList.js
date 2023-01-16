e16b1d686b1a5308403f76c66e636b4cb9603f0c
Rafael Leites Luchese <rlluchese@gmail.com>
2023-01-16 10:20:11 -0300

    Count using context example

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ books, onDelete, onEdit }) {
+
+    const {count, incrementCount} = useContext(BooksContext);
 
     const renderedBooks = books.map((book) => {
         return <BookShow key={ book.id } book={ book } onDelete={ onDelete } onEdit={ onEdit } />
@@ -8,6 +12,8 @@ function BookList({ books, onDelete, onEdit }) {
 
     return (
       <div className='book-list'>
+        { count }
+        <button onClick={ incrementCount }>Click</button>
         { renderedBooks }
       </div>
     );
